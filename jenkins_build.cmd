e84f5b6f6072c0f47738ea2583befeafe37d657e
Johnny Dallas <johnnydallas0308@gmail.com>
2018-04-12 01:26:27 +0000

    update deploy.rb and jenkins_build.cmd

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -1,12 +1,10 @@
-ECHO ON
+@ECHO ON
 set errorlevel=
-
-set FILENAME=%JOB_NAME%_%TAG%.zip
+set FILENAME=%TEMP%\%JOB_NAME%_%ENV%_%TAG%.zip
 
 rmdir /s /q dist
 rmdir /s /q x64
 rmdir /s /q Release
-del /Q /F %FILENAME%
 
 @REM build servers doesn't have this variable - we probably should be using vswhere
 set VS2017INSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
@@ -17,36 +15,48 @@ popd
 
 MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
 
-@if errorlevel 1 (
+if errorlevel 1 (
+  echo "Build Failed with %errorlevel%"
   exit /b %errorlevel%
 )
-@ECHO ON
 
 mkdir dist
-@if errorlevel 1 (
+if errorlevel 1 (
+  echo "mkdir dist failed with %errorlevel%"
   exit /b %errorlevel%
 )
 
 xcopy x64\Release\*.DLL dist
-@if errorlevel 1 (
+
+if errorlevel 1 (
+  echo "Failed xcopy x64\Release\*.DLL dist with %errorlevel%"
   exit /b %errorlevel%
 )
 
 xcopy x64\Release\*.pdb dist
-
-@if errorlevel 1 (
-  exit /b %errorlevel%
+if errorlevel 1 (
+    echo "Failed xcopy x64\Release\*.pdb dist with %errorlevel%"
+    exit /b %errorlevel%
 )
 
 
 "C:\Program Files\7-Zip\7z.exe" a -r %FILENAME% -w .\dist\* -mem=AES256
 
 @if errorlevel 1 (
-  exit /b %errorlevel%
+    echo "zip failed with %errorlevel%"
+    exit /b %errorlevel%
 )
 
-"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/bebo-gst-to-dshow/%FILENAME% --body %FILENAME%
-
-@if errorlevel 1 (
-  exit /b %errorlevel%
+if "%LIVE%" == "true" (
+    "C:\Python34\python.exe" "C:\w\jenkins_uploader.py" --project %JOB_NAME% --tag %TAG% --env %ENV%
+    @if errorlevel 1 (
+      echo "jenkins_upload failed with %errorlevel%"
+      exit /b %errorlevel%
+    )
+) else (
+    "C:\Python34\python.exe" "C:\w\jenkins_uploader.py" --project %JOB_NAME% --tag %TAG% --env %ENV% --no-deploy
+    @if errorlevel 1 (
+      echo "jenkins_upload failed with %errorlevel%"
+      exit /b %errorlevel%
+    )
 )
