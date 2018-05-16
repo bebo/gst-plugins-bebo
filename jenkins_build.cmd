caf63fe03b4d70e3161f8790f32f4c99085ab478
RowanMeara <rrm89@cornell.edu>
2018-05-16 14:45:02 -0700

    Include the MSBuild dll in the build script.

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -15,14 +15,33 @@ cmake --build . --config Release
   exit /b %errorlevel%
 )
 
+cd ..
+
+
+@REM build servers doesn't have this variable - we probably should be using vswhere
+set VS2017INSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+
+pushd .
+call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat" amd64
+popd
+
+MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
+
+if errorlevel 1 (
+  echo "Build Failed with %errorlevel%"
+  exit /b %errorlevel%
+)
+xcopy x64\Release\*.DLL .\build\gst\Release
+xcopy x64\Release\*.pdb .\build\gst\Release
+
+
+cd build
 set FILENAME=gst-bebo_%TAG%.zip
 "C:\Program Files\7-Zip\7z.exe" a -r ..\%FILENAME% -w .\gst\Release\* -mem=AES256
 
 @if errorlevel 1 (
   exit /b %errorlevel%
 )
-
-cd ..
 "C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/gst-bebo/%FILENAME% --body %FILENAME%
 
 @if errorlevel 1 (
