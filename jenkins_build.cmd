19ea3026a4d3df6ee225ca71f3ba8e43363f84a6
RowanMeara <rrm89@cornell.edu>
2018-05-16 13:42:12 -0700

    Update build script.

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -1,65 +1,32 @@
 @ECHO ON
 set errorlevel=
-set FILENAME=%TEMP%\%JOB_NAME%_%ENV%_%TAG%.zip
-set LEGACY_FILENAME=%JOB_NAME%_%TAG%.zip
 
-rmdir /s /q dist
-rmdir /s /q x64
-rmdir /s /q Release
-
-@REM build servers doesn't have this variable - we probably should be using vswhere
-set VS2017INSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
-
-pushd .
-call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat" amd64
-popd
-
-MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
-
-if errorlevel 1 (
-  echo "Build Failed with %errorlevel%"
-  exit /b %errorlevel%
-)
-
-mkdir dist
-if errorlevel 1 (
-  echo "mkdir dist failed with %errorlevel%"
-  exit /b %errorlevel%
-)
-
-xcopy x64\Release\*.DLL dist
-
-if errorlevel 1 (
-  echo "Failed xcopy x64\Release\*.DLL dist with %errorlevel%"
-  exit /b %errorlevel%
-)
-
-xcopy x64\Release\*.pdb dist
-if errorlevel 1 (
-    echo "Failed xcopy x64\Release\*.pdb dist with %errorlevel%"
-    exit /b %errorlevel%
-)
-
-
-"C:\Program Files\7-Zip\7z.exe" a -r %FILENAME% -w .\dist\* -mem=AES256
+mkdir build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" ..
 
 @if errorlevel 1 (
-    echo "zip failed with %errorlevel%"
-    exit /b %errorlevel%
+  exit /b %errorlevel%
 )
 
-"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/%JOB_NAME%/%LEGACY_FILENAME% --body %FILENAME%
+cmake --build . --config Release
 
-if "%LIVE%" == "true" (
-    "C:\Python34\python.exe" "C:\w\jenkins_uploader.py" --project %JOB_NAME% --tag %TAG% --env %ENV%
-    @if errorlevel 1 (
-      echo "jenkins_upload failed with %errorlevel%"
-      exit /b %errorlevel%
-    )
-) else (
-    "C:\Python34\python.exe" "C:\w\jenkins_uploader.py" --project %JOB_NAME% --tag %TAG% --env %ENV% --no-deploy
-    @if errorlevel 1 (
-      echo "jenkins_upload failed with %errorlevel%"
-      exit /b %errorlevel%
-    )
+@if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+set FILENAME=gst-bebo_%TAG%.zip
+set RELEASE_NAME=gst-bebo_%ENV%.zip
+"C:\Program Files\7-Zip\7z.exe" a -r ..\%FILENAME% -w .\gst\Release\* -mem=AES256
+
+@if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+cd ..
+"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/gst-cef/%FILENAME% --body %FILENAME%
+"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/gst-cef/%RELEASE_NAME% --body %FILENAME%
+
+@if errorlevel 1 (
+  exit /b %errorlevel%
 )
