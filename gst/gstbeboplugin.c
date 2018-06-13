f2ddd1102be33ad39b4a560b88e8cb1faba1a5a4
Jake Loo <slow-@live.com>
2018-06-13 03:39:15 -0700

    Initial commit for noisegate. Ported from libav-filters

--- gst/gstbeboplugin.c
+++ gst/gstbeboplugin.c
@@ -31,6 +31,7 @@
 #include "bebosquisher/gstglvideomixer.h"
 #include "bebosquisher/gstglstereomix.h"
 #include "bufferholder/gstbufferholder.h"
+#include "noisegate/gstaudionoisegate.h"
 
 __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
@@ -38,10 +39,9 @@ __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-
   if (load_nvenc_dlls()) {
-	gst_element_register(plugin, "d3dnvh264enc", GST_RANK_PRIMARY * 2,
-		gst_nv_h264_enc_get_type());
+    gst_element_register(plugin, "d3dnvh264enc", GST_RANK_PRIMARY * 2,
+        gst_nv_h264_enc_get_type());
   }
   gst_element_register(plugin, "dshowfiltersink",
     GST_RANK_NONE, GST_TYPE_SHM_SINK);
@@ -68,7 +68,10 @@ plugin_init (GstPlugin * plugin)
     GST_RANK_NONE, GST_TYPE_BUFFER_HOLDER)) {
     return FALSE;
   }
-
+  if (!gst_element_register(plugin, "noisegate",
+    GST_RANK_NONE, GST_TYPE_AUDIO_NOISE_GATE)) {
+    return FALSE;
+  }
   return TRUE;
 }
 
