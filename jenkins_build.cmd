b34e9a2d7b7f29f1c5be043a166295c198f274dd
Jake Loo <slow-@live.com>
2018-08-20 21:34:28 -0700

    Remove double building. Restructure plugins dist directory to include nacl-preview

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -18,32 +18,32 @@ cmake --build . --config Release
 cd ..
 
 
-@REM build servers doesn't have this variable - we probably should be using vswhere
-set VS2017INSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
-
-pushd .
-call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat" amd64
-popd
-
-MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
+:: @jake - it looks like we're already using cmake --build to build, we shouldn't need to build it again with msbuild
+::
+:: @REM build servers doesn't have this variable - we probably should be using vswhere
+:: set VS2017INSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+:: pushd .
+:: call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat" amd64
+:: popd
+:: MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
 
 if errorlevel 1 (
   echo "Build Failed with %errorlevel%"
   exit /b %errorlevel%
 )
-xcopy x64\Release\*.DLL .\build\gst\Release
-xcopy x64\Release\*.pdb .\build\gst\Release
 
+xcopy /Y .\build\gst\Release\*.dll %DIST_DIR%\gst-plugin\
+xcopy /Y .\build\gst\Release\*.pdb %DIST_DIR%\gst-plugin\
+xcopy /Y .\build\nacl-preview\Release\*.dll %DIST_DIR%\nacl-preview\
+xcopy /Y .\build\nacl-preview\Release\*.pdb %DIST_DIR%\nacl-preview\
 
-cd build
 set FILENAME=gst-plugins-bebo_%TAG%.zip
-"C:\Program Files\7-Zip\7z.exe" a -r ..\%FILENAME% -w .\gst\Release\* -mem=AES256
+"C:\Program Files\7-Zip\7z.exe" a -r %FILENAME% -w %DIST_DIR%\* -mem=AES256
 
 @if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-cd ..
 "C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/gst-plugins-bebo/%FILENAME% --body %FILENAME%
 
 @if errorlevel 1 (
